3dd427fa8f52fb924a03f8ed6243fa2df17945e0
Adal1942 <adautosombra1942@gmail.com>
2022-03-27 17:00:19 -0300

    adição do autoplay

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,9 @@ $('.sajid-1').owlCarousel({
     loop:true,
     margin:5,
     nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3500,
     responsive:{
         0:{
             items:1
@@ -18,6 +21,8 @@ $('.sajid-2').owlCarousel({
     loop:true,
     margin:10,
     nav:false,
+    autoplay: true,
+    autoplayTimeout: 4000,
     responsive:{
         0:{
             items:4
